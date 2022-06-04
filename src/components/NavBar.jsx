1301a35c317c7fad8b620135507031cab98b4802
Arfeen Mushtaque <arfeenmushtaque1998@gmail.com>
2022-06-04 12:59:03 +0530

    Last Touch

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,6 @@ import"./NavBar.css"
 export default function NavBar() {
    const uncheck = ()=>{
        document.getElementById("checker").checked = false
-     
    }
     return (
         <nav className='container'>
@@ -18,6 +17,7 @@ export default function NavBar() {
                <li onClick={uncheck}> <i className="far fa-address-card"></i> <a href="#ABOUT"> About </a></li>
                <li onClick={uncheck}> <i className="fas fa-sitemap"></i> <a href="#SKILLS"> Skills </a></li>
                <li onClick={uncheck}> <i className="fas fa-laptop"></i> <a href="#recent-work"> Projects </a></li>
+               <li onClick={uncheck}> <i className="fas fa-id-card"></i> <a href="#connect"> Contact </a></li>
                <li onClick={uncheck}> <i className="far fa-file-alt"></i><a target="_blank" rel='noreferrer' href="https://drive.google.com/file/d/1Fi9rUqmW4mFwrIrKcD4IDeOBIXucB7w3/view?usp=sharing"> Resume </a> </li>
            </ul>
         </nav>
