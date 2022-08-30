f98767fcf76a4968210b590f94cf94f1e85995c0
Arfeen Mushtaque <arfeenmushtaque1998@gmail.com>
2022-08-30 12:38:29 +0530

    Initial changes

--- README.md
+++ README.md
@@ -1,17 +1,2 @@
-# Portfolio
-url :(https://arfeen-portfolio.netlify.app/)
 
-This project was created using react and the page is responsive for all the screen sizes.
 
-# pic
-<img src='./Image/navbar.png' width='600px' height='400px'>
-<img src='./Image/Project.png' width='600px' height='400px'>
-
-# How to Start
-In the project directory, you can run:
-npm start
-Runs the app in the development mode.
-Open http://localhost:3000 to view it in your browser.
-
-The page will reload when you make changes.
-You may also see any lint errors in the console.
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import"./NavBar.css"
 export default function NavBar() {
    const uncheck = ()=>{
        document.getElementById("checker").checked = false
+     
    }
     return (
         <nav className='container'>
@@ -17,8 +18,7 @@ export default function NavBar() {
                <li onClick={uncheck}> <i className="far fa-address-card"></i> <a href="#ABOUT"> About </a></li>
                <li onClick={uncheck}> <i className="fas fa-sitemap"></i> <a href="#SKILLS"> Skills </a></li>
                <li onClick={uncheck}> <i className="fas fa-laptop"></i> <a href="#recent-work"> Projects </a></li>
-               <li onClick={uncheck}> <i className="fas fa-id-card"></i> <a href="#connect"> Contact </a></li>
-               <li onClick={uncheck}> <i className="far fa-file-alt"></i><a target="_blank" rel='noreferrer' href="https://drive.google.com/file/d/1Fi9rUqmW4mFwrIrKcD4IDeOBIXucB7w3/view?usp=sharing"> Resume </a> </li>
+               <li onClick={uncheck}> <i className="far fa-file-alt"></i><a target="_blank" rel='noreferrer' href="https://drive.google.com/file/d/1W3Figp3rvgaPaznQkRp6GZgsnAwiRsuV/view?usp=sharing"> Resume </a> </li>
            </ul>
         </nav>
     )
